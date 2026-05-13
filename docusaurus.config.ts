e4f3d9b2d9e702f9337cb7fcb77e4d9151a476ae
Toray Altas <toray.altas@gmail.com>
2026-05-13 18:52:58 -0400

    root the base url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -30,7 +30,7 @@ const config: Config = {
   url: 'https://zoo-code-org.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Zoo-Code-Docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config (if needed)
   organizationName: 'Zoo-Code-Org',
