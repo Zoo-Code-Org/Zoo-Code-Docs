8200a1305f8b9d1c3562af01e8292d97bbe1e506
Toray Altas <toray.altas@gmail.com>
2026-05-13 18:59:03 -0400

    update root url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -27,7 +27,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://zoo-code-org.github.io',
+  url: 'https://docs.zoocode.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
